b117ad5fcb8797059856fa18b7619ad36a8ebd63
Prakash Paudel <p.prakashpaudel35@gmail.com>
2026-07-04 17:26:41 +0530

    feat(route): setup the route path for the inventory,orders, customers , suppliers, categorries, reports, settings.

--- client/src/routes/routes.tsx
+++ client/src/routes/routes.tsx
@@ -1,3 +1,4 @@
+import Inventory from '@/features/inventory/Inventory'
 import Dashboard from '../features/dashboard/Dashboard'
 
 export const routes = [
@@ -5,5 +6,32 @@ export const routes = [
         path: '/',
         element: <Dashboard />,
     },
-
+    {
+        path: '/inventory',
+        element: <Inventory />,
+    },
+    {
+        path: '/orders',
+        element: <div>Orders</div>,
+    },
+    {
+        path: '/customers',
+        element: <div>Customers</div>,
+    },
+    {
+        path: '/suppliers',
+        element: <div>Suppliers</div>,
+    },
+    {
+        path: '/categories',
+        element: <div>Categories</div>,
+    },
+    {
+        path: '/reports',
+        element: <div>Reports</div>,
+    },
+    {
+        path: '/settings',
+        element: <div>Settings</div>,
+    },
 ]
