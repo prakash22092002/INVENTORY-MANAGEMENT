773c815bf5bd78236b152b275cd197d36dede384
Prakash Paudel <p.prakashpaudel35@gmail.com>
2026-07-04 17:25:56 +0530

    feat(inventory): setup the folder and the basic message for the inventory.

---
+++ client/src/features/inventory/Inventory.tsx
@@ -0,0 +1,12 @@
+
+
+const Inventory = () => {
+    return (
+        <div className="flex flex-col items-center justify-center">
+            <h1 className="text-3xl font-bold">Inventory</h1>
+            <p className="text-lg text-gray-600">Manage your inventory products here.</p>
+        </div>
+    )
+}
+
+export default Inventory
