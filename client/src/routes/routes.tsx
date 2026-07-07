b3f65f82144c238f17fba405e755e80c3a9843fd
Prakash Paudel <p.prakashpaudel35@gmail.com>
2026-07-07 21:37:58 +0530

    fix(route): added the routes for the login and sign up page.

--- client/src/routes/routes.tsx
+++ client/src/routes/routes.tsx
@@ -1,11 +1,26 @@
+import Login from '@/features/auth/Login'
+import SignUp from '@/features/auth/SignUp'
 import Inventory from '@/features/inventory/Inventory'
 import ProductPreview from '@/features/inventory/ProductPreview'
 import Dashboard from '../features/dashboard/Dashboard'
 import Categories from '@/features/categories/Categories'
 import Orders from '@/features/orders/Orders'
 import OrderPreview from '@/features/orders/OrderPreview'
+import Settings from '@/features/settings/Settings'
+import Customers from '@/features/customers/Customers'
+import CustomerPreview from '@/features/customers/CustomerPreview'
 
 export const routes = [
+    {
+        path: '/login',
+        element: <Login />,
+        hideNavbar: true,
+    },
+    {
+        path: '/signup',
+        element: <SignUp />,
+        hideNavbar: true,
+    },
     {
         path: '/',
         element: <Dashboard />,
@@ -28,7 +43,11 @@ export const routes = [
     },
     {
         path: '/customers',
-        element: <div>Customers</div>,
+        element: <Customers />,
+    },
+    {
+        path: '/customers/:id',
+        element: <CustomerPreview />,
     },
     {
         path: '/suppliers',
@@ -44,6 +63,6 @@ export const routes = [
     },
     {
         path: '/settings',
-        element: <div>Settings</div>,
+        element: <Settings />,
     },
 ]
