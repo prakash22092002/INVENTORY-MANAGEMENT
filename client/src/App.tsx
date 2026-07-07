f91b65188d13ba4dc9344e63295e9a2ac3c5241b
Prakash Paudel <p.prakashpaudel35@gmail.com>
2026-07-07 21:31:53 +0530

    fix(app): handled the path for the auth page.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,44 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useEffect } from 'react'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import { routes } from './routes/routes.tsx'
 import { Navbar } from './features/navbar/Navbar'
+import { useAuthStore } from './store/authStore'
+import ProtectedRoute from './components/common/ProtectedRoute'
 import './App.css'
+
+function AppContent() {
+  const { pathname } = useLocation()
+  const { checkAuth } = useAuthStore()
+
+  useEffect(() => {
+    checkAuth()
+  }, [checkAuth])
+
+  const hideNavbar = pathname === '/login' || pathname === '/signup'
+
+  return (
+    <div className="mx-auto min-h-screen max-w-7xl px-3 pt-3 sm:px-4 sm:pt-4">
+      {!hideNavbar && <Navbar />}
+      <main className={hideNavbar ? '' : 'mt-4 px-1 pb-8 sm:mt-6 sm:px-2'}>
+        <Routes>
+          {routes?.map((route) => {
+            const element = route.hideNavbar ? (
+              route.element
+            ) : (
+              <ProtectedRoute>{route.element}</ProtectedRoute>
+            )
+            return <Route key={route.path} path={route.path} element={element} />
+          })}
+        </Routes>
+      </main>
+    </div>
+  )
+}
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="mx-auto min-h-screen max-w-7xl px-3 pt-3 sm:px-4 sm:pt-4">
-        <Navbar />
-        <main className="mt-4 px-1 pb-8 sm:mt-6 sm:px-2">
-          <Routes>
-            {routes?.map((route) => (
-              <Route key={route.path} path={route.path} element={route.element} />
-            ))}
-          </Routes>
-        </main>
-      </div>
+      <AppContent />
     </BrowserRouter>
   )
 }
