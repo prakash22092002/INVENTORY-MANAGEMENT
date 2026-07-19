2cf7e73ff38627b1c1ba3c79c58d165042071149
Prakash Paudel <p.prakashpaudel35@gmail.com>
2026-07-19 20:53:16 +0530

    refactor: remove unused useNavigate hook from SignUp component

--- client/src/features/auth/SignUp.tsx
+++ client/src/features/auth/SignUp.tsx
@@ -1,5 +1,5 @@
 import { useState, type FormEvent } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { Eye, EyeOff, Mail, Lock, User, UserPlus, Sparkles } from 'lucide-react'
 import { cn } from '@/lib/utils'
 
