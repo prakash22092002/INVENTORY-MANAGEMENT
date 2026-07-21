a28bdab482a64505f40952237b9d464c4b92342a
Prakash Paudel <p.prakashpaudel35@gmail.com>
2026-07-21 11:01:05 +0530

    feat: update CORS origin configuration, rename login token key, and force DNS resolution for database connections

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,7 +11,10 @@ const app = express();
 
 // Global Middlewares
 app.use(helmet());
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+
+app.use(cors({ origin: process.env.CLIENT_DEV_URL, credentials: true }));
+
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
